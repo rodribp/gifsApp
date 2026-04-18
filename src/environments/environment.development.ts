22bfe7e564e55fae2ff195e701bf71f7521d86dc
rodribp <rodrigobonilla177@gmail.com>
2026-04-18 13:14:56 -0600

    chore: remove exposed development environment file

--- src/environments/environment.development.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  appName: 'Gifs',
-  appName2: 'App',
-  appSlogan: 'Maneja tus gifs (Desarrollo)',
-  giphyApiKey: '',
-  giphyUrl: 'https://api.giphy.com/v1'
-};
